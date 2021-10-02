7bd6b2fed3ca27b8ff0a37faf487a90d33325032
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-02 13:56:35 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppRouter } from "./router/AppRouter";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <AppRouter />
+    </I18nextProvider>
   );
 }
 
