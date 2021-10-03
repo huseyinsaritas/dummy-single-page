39378571d1c192f2dd43dcadc28adec6a9e6f450
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-04 02:33:06 +0300

    Localization

--- src/components/Localization/Localization.tsx
+++ src/components/Localization/Localization.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import { Dropdown } from "rsuite";
+import { useDispatch } from "react-redux";
+import { Icon } from "rsuite";
 import { languages } from "../../constants/languages";
 import { setLanguage } from "../../redux/action/user";
-import { UserState } from "../../redux/model/user";
+import "./Localization.scss";
 
 type Props = {
   currentLanguage: string;
@@ -15,21 +15,26 @@ export const Localization: React.FC<Props> = ({ currentLanguage }) => {
 
   const { i18n } = useTranslation();
 
-  const language = useSelector((state: UserState) => state.language);
-
   const onSelect = (value: string) => {
+    console.log(value);
+
     localStorage.setItem("i18nextLng", value);
     dispatch(setLanguage(value));
     i18n.changeLanguage(value);
   };
 
   return (
-    <Dropdown trigger="hover" activeKey={language} title={currentLanguage.toUpperCase()}>
-      {languages.map((language) => (
-        <Dropdown.Item key={language.key} onSelect={() => onSelect(language.key)}>
-          {language.value}
-        </Dropdown.Item>
-      ))}
-    </Dropdown>
+    <div className="dropdown">
+      <button className="dropbtn">
+        {currentLanguage.toUpperCase()} <Icon icon="down" />
+      </button>
+      <div className="dropdown-content">
+        {languages.map((language) => (
+          <a key={language.key} onClick={() => onSelect(language.key)}>
+            {language.value}
+          </a>
+        ))}
+      </div>
+    </div>
   );
 };
