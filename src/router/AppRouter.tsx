d5fc330f3b6d99bf3434d3196bd7567734399314
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-03 17:14:57 +0300

    AppRouter: Footer added

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import { MainNavbar } from "../components/Navbar/Navbar";
+import { Footer } from "../components/Footer/Footer";
+import { Navbar } from "../components/Navbar/Navbar";
 import { ROUTE_PATH } from "../enum/ROUTE_PATH";
 import { ContactUs } from "../pages/ContactUs/ContactUs";
 import { Home } from "../pages/Home/Home";
@@ -8,13 +9,12 @@ import { Home } from "../pages/Home/Home";
 export const AppRouter = () => {
   return (
     <Router>
-      <MainNavbar />
+      <Navbar />
       <Switch>
         <Route path={ROUTE_PATH.HOME} exact component={Home} />
         <Route path={ROUTE_PATH.CONTACT} component={ContactUs} />
-        {/* <Route path={ROUTE_PATH.LOGIN} component={Login} />*/}
       </Switch>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 };
