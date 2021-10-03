39e649820426047116a193c7894d1e87efebf11e
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-04 02:32:56 +0300

    Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { ControlLabel, Form, FormControl, FormGroup } from "rsuite";
-import { login } from "../../redux/action/user";
+import { setUser } from "../../redux/action/user";
 import { Modal } from "../Modal/Modal";
 import "./Login.scss";
 
@@ -12,33 +11,68 @@ type Props = {
 };
 
 export const LoginModal: React.FC<Props> = ({ show, close }) => {
-  const [user, setUser] = useState({ username: "", email: "", password: "" });
+  const [loginFormData, setLoginFormData] = useState({ username: "", email: "", password: "" });
   const { t } = useTranslation();
 
   const dispatch = useDispatch();
 
   const userLogin = () => {
-    localStorage.setItem("user", JSON.stringify(user));
-    dispatch(login({ name: user.username, email: user.email }));
+    localStorage.setItem("user", JSON.stringify(loginFormData));
+    dispatch(setUser({ name: loginFormData.username, email: loginFormData.email }));
     close();
   };
 
   return (
     <Modal show={show} close={close} submit={userLogin}>
-      <Form fluid>
-        <FormGroup controlId="name-1">
-          <ControlLabel>{t("username")}</ControlLabel>
-          <FormControl name="name" value={user.username} onChange={(value: string) => setUser({ ...user, username: value })} />
-        </FormGroup>
-        <FormGroup controlId="email-1">
-          <ControlLabel>{t("email")}</ControlLabel>
-          <FormControl name="email" type="email" value={user.email} onChange={(value: string) => setUser({ ...user, email: value })} />
-        </FormGroup>
-        <FormGroup controlId="password-1">
-          <ControlLabel>{t("password")}</ControlLabel>
-          <FormControl name="password" type="password" value={user.password} onChange={(value: string) => setUser({ ...user, password: value })} />
-        </FormGroup>
-      </Form>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username" className="control-label">
+            {t("username")}
+          </label>
+          <div className="control-wrapper">
+            <input
+              name="username"
+              id="username"
+              type="text"
+              className="form-input"
+              value={loginFormData.username}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLoginFormData({ ...loginFormData, username: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email" className="control-label">
+            {t("email")}
+          </label>
+          <div className="control-wrapper">
+            <input
+              name="email"
+              id="email"
+              type="email"
+              className="form-input"
+              value={loginFormData.email}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLoginFormData({ ...loginFormData, email: event.target.value })}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password" className="control-label">
+            {t("password")}
+          </label>
+          <div className="control-wrapper">
+            <input
+              name="password"
+              id="password"
+              type="password"
+              className="form-input"
+              value={loginFormData.password}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setLoginFormData({ ...loginFormData, password: event.target.value })}
+            />
+          </div>
+        </div>
+      </form>
     </Modal>
   );
 };
