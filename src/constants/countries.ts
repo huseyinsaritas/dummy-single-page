7b13896e57a2213f2d0d24ff459fdbf7e00ba987
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-04 02:34:23 +0300

    countryList

--- src/constants/countries.ts
+++ src/constants/countries.ts
@@ -1,10 +1,32 @@
-export const countries = [
-  { id: "TR", name: "Turkey" },
-  { id: "US", name: "United States of America" },
-  { id: "GB", name: "United Kingdom" },
-  { id: "DE", name: "Germany" },
-  { id: "SE", name: "Sweden" },
-  { id: "KE", name: "Kenya" },
-  { id: "BR", name: "Brazil" },
-  { id: "ZW", name: "Zimbabwe" },
-];
+import i18n from "../i18n";
+
+type Country = {
+  id: string;
+  name: string;
+};
+
+export const countryList = (): Country[] => {
+  if (i18n.language === "tr") {
+    return [
+      { id: "TR", name: "Türkiye" },
+      { id: "US", name: "Amerika Birleşik Devletleri" },
+      { id: "GB", name: "Birleşik Krallık" },
+      { id: "DE", name: "Almanya" },
+      { id: "SE", name: "İsveç" },
+      { id: "KE", name: "Kenya" },
+      { id: "BR", name: "Brezilya" },
+      { id: "ZW", name: "Zimbabve" },
+    ];
+  } else {
+    return [
+      { id: "TR", name: "Turkey" },
+      { id: "US", name: "United States of America" },
+      { id: "GB", name: "United Kingdom" },
+      { id: "DE", name: "Germany" },
+      { id: "SE", name: "Sweden" },
+      { id: "KE", name: "Kenya" },
+      { id: "BR", name: "Brazil" },
+      { id: "ZW", name: "Zimbabwe" },
+    ];
+  }
+};
